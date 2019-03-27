4323668fdd647bcb372b171770f95bf44d09f201
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-03-27 16:00:08 +0800

    Constructed the weight and bias matrix

--- HW1/dnn_1.py
+++ HW1/dnn_1.py
@@ -1,20 +1,20 @@
 """
-env: Python 3.7 on Ubuntu 18.04.2
-cpu: Core i5 - 7500, gpu: NVIDIA GTX 1070
+Env: Python 3.7 on Ubuntu 18.04.2
 """
 import numpy as np
 import matplotlib as plt
-import csv
 import random as rd
+import csv, os, struct
 
 ################# GLOBAL DEF ###########
 
-F_NAME = "titanic.csv"
+F_NAME = 'titanic.csv'
 N_TRAIN_DATA = 800
 N_TEST_DATA = 91
-N_HIDDEN_LAYER = 3
-N_UNITS = [6, 3, 3, 2]
+N_DIM = 6
 
+N_UNIT_1 = 6 # unit for layer 1
+N_UNIT_2 = 6 # unit for layer 2
 ################# FILE IO ##############
 
 def file_IO():
@@ -27,14 +27,19 @@ def file_IO():
         test_data = rows[N_TRAIN_DATA + 1:]
 
     return label, train_data, test_data
-################## SGD #################
+################# SGD #################
 
 
 ################## NN ##################
 class NN(object):
     def __init__(self, sizes):
         self.sizes = sizes
-        self.num_layers = N_HIDDEN_LAYER
-        self.w = # weight of the layer
+        self.num_layers = len(sizes)
+        self.w = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]# weight of the layer
+        self.b = [np.random.randn(y, 1) for y in sizes[1:]]
+
 if __name__ == '__main__':
     label, train_data, test_data = file_IO()
+    net = NN([N_DIM , N_UNIT_1, 1])
+    print('Weight matrix: ', net.w)
+    print('\nBias matrix: ', net.b)
