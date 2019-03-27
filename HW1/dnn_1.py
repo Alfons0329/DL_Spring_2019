c49646bd1d7d01b172dec18e54f08a94e065622f
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-03-27 22:06:42 +0800

    Doing back propogation

--- HW1/dnn_1.py
+++ HW1/dnn_1.py
@@ -14,7 +14,9 @@ N_TEST_DATA = 91
 N_DIM = 6
 
 N_UNIT_1 = 6 # unit for layer 1
-N_UNIT_2 = 6 # unit for layer 2
+N_UNIT_2 = 3 # unit for layer 2
+N_EPOCH_LIMIT = 100
+LEARNING_RATE = 0.50
 ################# FILE IO ##############
 
 def file_IO():
@@ -38,8 +40,20 @@ class NN(object):
         self.w = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]# weight of the layer
         self.b = [np.random.randn(y, 1) for y in sizes[1:]]
 
+    def sigmoid(self, z):
+        return 1.0/(1.0 + np.exp(-z))
+
+    def sigmoid_prime(self, z):
+        return self.sigmoid(z) * (1 - self.sigmoid(z))
+
+    def forward(self, x):
+        for b, w in zip(self.b, self.w):
+            x = self.sigmoid(np.dot(w, x) + b)
+
+        return x
+
 if __name__ == '__main__':
     label, train_data, test_data = file_IO()
-    net = NN([N_DIM , N_UNIT_1, 1])
-    print('Weight matrix: ', net.w)
-    print('\nBias matrix: ', net.b)
+    net = NN([N_DIM , N_UNIT_1, N_UNIT_2, 1])
+    #print('Weight matrix: ', net.w)
+    #print('\nBias matrix: ', net.b)
