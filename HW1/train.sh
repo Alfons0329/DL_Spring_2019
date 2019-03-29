4437c3c1e46a7a568e2dcea48fe7c4e91bbc6e7b
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-03-30 03:32:26 +0800

    Autotrain

---
+++ HW1/train.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for batch_size in 4 8 16 32
+do
+    for learning_rate in 0.000001 0.00001 0.0001 0.001 0.01 0.1 0.5 1 2 3 5
+    do
+        echo $batch_size $learning_rate
+        python3 dnn_1.py $1\_$batch_size\_$learning_rate $batch_size $learning_rate
+    done
+done
