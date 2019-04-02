3593d70176f8ea098d9ce0c8fa337ac0e374831e
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-04-02 16:17:04 +0800

    0402 3rd test part updated, now doing problem 2

--- HW1/train.sh
+++ HW1/train.sh
@@ -3,7 +3,7 @@ read -p "1: All combination of batch, 2: Current best " sel
 if [ $sel -eq 1 ];
 then
     batch_size=$2
-    for learning_rate in 0.000005 0.000008 0.00001 0.00005 0.00008
+    for learning_rate in 0.000001 0.000005 0.000008 0.00001 0.00005 0.00008
     do
         echo $learning_rate
         python3 dnn_1.py $1\_$batch_size\_$learning_rate $batch_size $learning_rate
