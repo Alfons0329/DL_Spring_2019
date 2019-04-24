215d5d885a73a0407f7516f10d60a432414d9171
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-04-25 00:11:26 +0800

    Update some preprocessing, validate tomorrow

---
+++ HW2/train.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+read -p "1: All combination of batch, 2: Current best " sel
+
+if [ $sel -eq 1 ];
+then
+    for batch_size in 64 128 256 1024 4096
+    do
+        for learning_rate in  0.0000001 0.0000005 0.000001 0.000005 0.00001
+        do
+            echo $learning_rate
+            python3 cnn.py $1\_$batch_size\_$learning_rate $batch_size $learning_rate
+        done
+    done
+else
+    echo "None"
+    #python3 dnn_1.py $1_16_0.00001 16 0.00001
+fi
+
+mkdir -p $1 #_P3
+mv $1*\.png $1/ #_P3/
