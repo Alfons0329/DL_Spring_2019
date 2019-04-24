89f5f4340bbf857bfd1f7abba4c8b3fad6c4e380
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-04-25 00:08:01 +0800

    Update some preprocessing, validate tomorrow

--- HW2/cnn.py
+++ HW2/cnn.py
@@ -1,3 +1,4 @@
+############# IMPORT MODULE #########
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -5,28 +6,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import regex as re
 
-
+############# MY PROPROCESSS #########
+import preprocessing_1
 ############# GLOBAL DEF ####### #####
 classes = ('dog', 'horse', 'elephant', 'butterfly', 'chicken', 'cat', 'cow', 'sheep', 'spider', 'squirrel')
 train_path = 'animal/train/'
 valid_path = 'animal/val/'
 
-
 ############# FILE IO ################
-def imshow(img):
-    img = img / 2 + 0.5
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show
 
-def file_IO():
-    train_input = torchvision.datasets.ImageFolder(root = train_path, transform = transforms.ToTensor())
-    train_loader = torch.utils.data.DataLoader(train_input, batch_size = 128, num_workers = 0, shuffle = False)
-    dataiter = iter(train_loader)
-    images = dataiter.next()
-    print(type(dataiter), type(train_loader))
-    imshow(images)
-    return train_loader
 
 if __name__ == '__main__':
-    train_loader = file_IO()
+	train_input, test_input = preprocessing_1.IO_preprocess()
--- HW2/preprocessing_1.py
+++ HW2/preprocessing_1.py
@@ -1,3 +1,24 @@
 import torch
 import torchvision
+import torchvision.transforms as transforms
 import cv2
+import os, sys
+
+########### GLOBAL DEF ############
+
+train_path = 'animal/train/'
+valid_path = 'animal/val/'
+N_BATCH_SIZE = int(sys.argv[2])
+
+########### IO PREPROCESS ##########
+def IO_preprocess():
+    # image rgb range [0, 255] -> [0.0, 1.0] and -> [-1, 1]
+    my_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    train_input = torchvision.datasets.ImageFolder(root = train_path, transform = my_transform)
+    train_loader = torch.utils.data.DataLoader(train_input, batch_size = N_BATCH_SIZE, num_workers = 0, shuffle = False)
+
+    test_input = torchvision.datasets.ImageFolder(root = valid_path, transform = my_transform)
+    test_loader = torch.utils.data.DataLoader(train_input, batch_size = N_BATCH_SIZE, num_workers = 0, shuffle = False)
+    return train_loader, test_loader
+
