4e97c1416e09621d73efdf7c99ae79b34dc09d3a
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-04-26 13:08:14 +0800

    Using VGG 16 for this homework

--- HW2/cnn.py
+++ HW2/cnn.py
@@ -15,6 +15,7 @@ import preprocessing_1 as pre
 
 ############# GLOBAL DEF #############
 classes = ('dog', 'horse', 'elephant', 'butterfly', 'chicken', 'cat', 'cow', 'sheep', 'spider', 'squirrel')  # represent in [0, 9] will be better
+net_arch16 = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M5', "FC1", "FC2", "FC"]
 train_path = 'animal/train/'
 valid_path = 'animal/val/'
 
@@ -25,10 +26,17 @@ N_STRID_SIZE = int(sys.argv[3])
 N_TRAIN_DATA = 10000
 
 ############# GLOBAL DEF #############
-class Net(nn.Module):
-    def __init__(self):
-        ########## NN ARCHITECTURE ### 20190426 todo
-        super(Net, self).__init__() ## inherit from father nn.module object
+class vgg_net(nn.Module):
+    def __init__(self, net_arch, num_classes):
+        ########## NN ARCHITECTURE ###
+        super(vgg_net, self).__init__() # inherit from father nn.module object
+        self.num_classes = num_classes
+        layers = []
+        in_channels = 3
+        for one_layer in net_arch:
+            if arch == 'M': # the max pooling layer
+                layers.append(nn.MaxPool2d(kernel_size = 2, stride = 2))
+
 
 ############# CUUUUUUUDA #############
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
