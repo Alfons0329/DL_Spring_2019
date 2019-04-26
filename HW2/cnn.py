47a0d8b255e891f1d088dc27a972daf9634297f7
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-04-26 13:40:54 +0800

    Modyfying the VGG net

--- HW2/cnn.py
+++ HW2/cnn.py
@@ -10,12 +10,15 @@ import regex as re
 import os, sys
 
 from PIL import Image
+
 ############# MY PREPROCESSS #########
 import preprocessing_1 as pre
 
 ############# GLOBAL DEF #############
-classes = ('dog', 'horse', 'elephant', 'butterfly', 'chicken', 'cat', 'cow', 'sheep', 'spider', 'squirrel')  # represent in [0, 9] will be better
-net_arch16 = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M5', "FC1", "FC2", "FC"]
+# define the classes, represent in [0, 9] will be better
+classes = ('dog', 'horse', 'elephant', 'butterfly', 'chicken', 'cat', 'cow', 'sheep', 'spider', 'squirrel')
+# define the VGG 16 layer architecture
+net_one_layer16 = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M5', "FC1", "FC2", "FC"]
 train_path = 'animal/train/'
 valid_path = 'animal/val/'
 
@@ -27,16 +30,35 @@ N_TRAIN_DATA = 10000
 
 ############# GLOBAL DEF #############
 class vgg_net(nn.Module):
-    def __init__(self, net_arch, num_classes):
-        ########## NN ARCHITECTURE ###
+    def __init__(self, net_one_layer, num_classes):
+        ########## NN one_layerITECTURE ###
         super(vgg_net, self).__init__() # inherit from father nn.module object
         self.num_classes = num_classes
         layers = []
         in_channels = 3
-        for one_layer in net_arch:
-            if arch == 'M': # the max pooling layer
+        for one_layer in net_one_layer:
+            if one_layer == 'M': # the max pooling layer
                 layers.append(nn.MaxPool2d(kernel_size = 2, stride = 2))
+            elif one_layer == 'M5':
+                layers.append(nn.MaxPool2d(kernel_size=3, stride=1, padding=1))
+            elif one_layer == "FC1":
+                layers.append(nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=3, padding=6, dilation=6))
+                layers.append(nn.ReLU(inplace=True))
+            elif one_layer == "FC2":
+                layers.append(nn.Conv2d(1024,1024, kernel_size=1))
+                layers.append(nn.ReLU(inplace=True))
+            elif one_layer == "FC":
+                layers.append(nn.Conv2d(1024,self.num_classes, kernel_size=1))
+            else:
+                layers.append(nn.Conv2d(in_channels=in_channels, out_channels=one_layer, kernel_size=3, padding=1)
+                layers.append(nn.ReLU(inplace=True))
+                in_channels=one_layer
 
+
+        self.vggnet = nn.ModuleList(layers)
+
+    def forward(x): ######### 0426 Todo ###
+        return 0
 
 ############# CUUUUUUUDA #############
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -49,14 +71,14 @@ def img_show(img):
     plt.show()
 
 if __name__ == '__main__':
-    train_input, test_input = pre.IO_preprocess(N_BATCH_SIZE)
-    train_input_label, test_input_label = pre.add_label()
-    print(len(train_input), len(test_input))
+train_input, test_input = pre.IO_preprocess(N_BATCH_SIZE)
+train_input_label, test_input_label = pre.add_label()
+print(len(train_input), len(test_input))
 
-    cnt = 0
-    for img, label in train_input:
-        print(train_input_label[cnt: (cnt + 1) * 1000])
-        print(test_input_label[cnt: (cnt + 1) * 400])
-        #mg_show(torchvision.utils.make_grid(img))
-        cnt += 1
+cnt = 0
+for img, label in train_input:
+print(train_input_label[cnt: (cnt + 1) * 1000])
+print(test_input_label[cnt: (cnt + 1) * 400])
+#mg_show(torchvision.utils.make_grid(img))
+cnt += 1
 
