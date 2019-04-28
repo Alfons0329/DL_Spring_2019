468593158142ebd9a62b6a1d88e1aa6583821ceb
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-04-28 23:25:25 +0800

    New VGG model, try running on GPU

--- HW2/cnn.py
+++ HW2/cnn.py
@@ -43,13 +43,13 @@ class VGG(nn.Module):
         super(VGG, self).__init__()
         self.features = features
         self.classifier = nn.Sequential(
-                nn.Dropout(),
-                nn.Linear(512, 512),
+                nn.Linear(512 * 7 * 7, 4096),
                 nn.ReLU(True),
                 nn.Dropout(),
-                nn.Linear(512, 512),
+                nn.Linear(4096, 4096),
                 nn.ReLU(True),
-                nn.Linear(512, 10),
+                nn.Dropout(),
+                nn.Linear(4096, len(classes)),
                 )
         # Initialize weights
         for m in self.modules():
@@ -61,8 +61,11 @@ class VGG(nn.Module):
 
     def forward(self, x):
         x = self.features(x)
+        print('forward x shape ', x.size())
         x = x.view(x.size(0), -1) # flatten to be input to classifier
+        print('after view x shape ', x.size())
         x = self.classifier(x)
+        print('after classifier x shape ', x.size())
         return x
 
 
@@ -81,19 +84,30 @@ def make_layers(arch, batch_norm=False):
             in_channels = v
     return nn.Sequential(*layers)
 
-############ TRAIN NETWORK #######
+############# TRAIN NN #################
 
-def train(train_loader, model, criterion, optimizer, cur_epoch):
+def train(train_loader, model, criterion, optimizer, cur_epoch, device):
     print('\nEpoch: %d' % cur_epoch)
-    train_loss = 0
+    train_loss = 0.0
     correct = 0
     total = 0
+    batch_cnt = 0
 
-    for i, (input, target) in enumerate(train_loader):
-        target = target.cuda(async = True)
-        input_var = torch.autograd.Variable(input).cuda()
-        target_var = torch.autograd.Variable(target)
+    for i, data in enumerate(train_loader, 0):
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
 
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+
+        if i % N_BATCH_SIZE == 0:
+            print('[Epoch %5d batch %5d] CE loss: %.3f\n' %(cur_epoch, batch_cnt, train_loss / N_BATCH_SIZE))
+            train_loss = 0.0
 
 ############# DEBUG SHOW #############
 def img_show(img):
@@ -105,19 +119,21 @@ def img_show(img):
 
 if __name__ == '__main__':
     ############# LOAD DATASET ###########
-    train_loader, test_loader = pre.IO_preprocess(N_BATCH_SIZE, False) # make them together
+    train_loader, test_loader = pre.IO_preprocess(N_BATCH_SIZE, True) # make them together
     print(len(train_loader), len(test_loader))
 
     ############# CUUUUUUUDA #############
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
     model = VGG(make_layers(VGG16_arch))
     if device == 'cuda':
         print('Train with CUDA ')
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         model.features = torch.nn.DataParallel(model.features)
         model.cuda()
         criterion = nn.CrossEntropyLoss().cuda()
 
     optimizer = optim.SGD(model.parameters(), lr = N_LEARN_RATE, momentum = 0.9)
-
-    #for cur_epoch in range(0, N_EPOCH_LIMIT):
-    #    train(train_loader, model, criterion, optimizer, cur_epoch)
+    print('Start training, batch = %5d, total epoch = %5d\n'%(N_BATCH_SIZE, N_EPOCH_LIMIT))
+    for cur_epoch in range(0, N_EPOCH_LIMIT):
+        train(train_loader, model, criterion, optimizer, cur_epoch, device)
