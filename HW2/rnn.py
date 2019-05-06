dac4d50ea2d4a445ec431857e0ab3184d5b35af9
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-05-06 19:07:50 +0800

    Doing worb embedding in RNN

--- HW2/rnn.py
+++ HW2/rnn.py
@@ -12,7 +12,6 @@ import regex as re
 import os, sys, math
 
 ############# GLOBAL DEF #############
-
 F_NAME_ACCEPT = 'ICLR_accepted.xlsx'
 F_NAME_REJECT = 'ICLR_rejected.xlsx'
 
@@ -25,17 +24,48 @@ N_LEARN_RATE = float(sys.argv[1])
 N_BATCH_SIZE = int(sys.argv[2])
 adaptive_lr = str(sys.argv[3])
 
+N_VEC_SIZE = 10
 N_EPOCH_LIMIT = 200
 N_TEST_SIZE = 50
+
+word_dict = dict()
+dict_cnt = 1
 
 ############# PARSE XLSX #############
 def parse_xls(f_name):
     df = pd.read_excel(f_name)
     df = df[[0]]
-    print(df)
 
-    return df, df
+    return df[N_TEST_SIZE:], df[0: N_TEST_SIZE]
 
+############# WORD MBED AND DICT #####
+def word_embedding(data):
+    return embedded_data
+
+def build_dict(data):
+    for each_sentence in data:
+        print(each_sentence)
+        to_split = str(each_sentence)
+        to_split = to_split.split()
+        for each_word in to_split:
+            if each_word not in word_dict:
+                global dict_cnt
+                word_dict[each_word] = dict_cnt
+                dict_cnt += 1
 
 if __name__ == '__main__':
     train_input_acc, test_input_acc = parse_xls(F_NAME_ACCEPT)
+    train_input_rej, test_input_rej = parse_xls(F_NAME_REJECT)
+
+    train_input_acc = train_input_acc.values.tolist()
+    test_input_acc = test_input_acc.values.tolist()
+    train_input_rej = train_input_rej.values.tolist()
+    test_input_rej = test_input_rej.values.tolist()
+
+    together = [train_input_acc, train_input_rej]
+
+    for each_set in together:
+        build_dict(each_set)
+
+    print(type(word_dict), word_dict)
+
