daa643288d0537c377dff829be360e5af70fb27e
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-05-25 23:42:10 +0800

    HW3 prototype

---
+++ HW3/VAE/vae.py
@@ -0,0 +1,37 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.nn.init as init
+import matplotlib.pyplot as plt
+
+import numpy as np
+import os, sys, math
+
+from PIL import Image
+
+class VAE(nn.Module):
+    def __init__(self):
+        return 0
+
+    def encode(self, x):
+        return 0
+
+    def reparameterize(self):
+        return 0
+
+    def decode(self):
+        return 0
+
+    def get_latent_var(self):
+        return 0
+
+    def forward(self, x):
+        return 0
+
+model = VAE()
+
+def train():
+    return 0
