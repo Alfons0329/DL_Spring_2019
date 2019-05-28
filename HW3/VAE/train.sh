a30cad17c95a70eb9f2b1e677784675ef1b6a5bb
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-05-28 16:08:13 +0800

    VAE tuning for autogenerate image and organize

---
+++ HW3/VAE/train.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+for batch_size in 128
+do
+    for lr in 0.00001 0.1
+    do
+        mkdir -p curves/$1
+        mkdir -p reconstructed/$1
+        mkdir -p generated/$1
+
+        python3 vae.py --lr $lr --batch_size $batch_size
+
+        mv *LC\.png curves/$1
+        mv *\_x\.png curves/$1
+        mv *\_gen\.png curves/$1
+
+        rm -rf *.pt best_loss.txt
+    done
+done
