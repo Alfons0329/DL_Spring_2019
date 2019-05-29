adf555238518a701829fa62d21cddb38f0da82dd
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-05-30 00:28:15 +0800

    CNN type work, but too Korean

--- HW3/VAE/vae_cnn.py
+++ HW3/VAE/vae_cnn.py
@@ -6,11 +6,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.nn.init as init
+from torch.autograd import Variable
 import matplotlib.pyplot as plt
 
 import numpy as np
 import os, sys, math
-
 import argparse
 
 from PIL import Image
@@ -33,7 +33,7 @@ N_IMG_SIZE = 0
 N_FC1_SIZE = 64
 N_FC2_SIZE = 16
 
-N_EPOCH_LIMIT = 200
+N_EPOCH_LIMIT = 1000
 N_LEARN_RATE = args.lr
 N_BATCH_SIZE = args.batch_size
 N_IMG_SIZE = args.img_size
@@ -87,11 +87,11 @@ class VAE(nn.Module):
             nn.ReLU(),
             flatten()
         )
-        
+
         self.fc1 = nn.Linear(h_dim, z_dim)
         self.fc2 = nn.Linear(h_dim, z_dim)
         self.fc3 = nn.Linear(z_dim, h_dim)
-        
+
         self.decoder = nn.Sequential(
             unflatten(),
             nn.ConvTranspose2d(h_dim, 128, kernel_size = 5, stride = 2),
@@ -101,20 +101,32 @@ class VAE(nn.Module):
             nn.ConvTranspose2d(64, 32, kernel_size = 6, stride = 2),
             nn.ReLU(),
             nn.ConvTranspose2d(32, image_channels, kernel_size = 6, stride = 2),
-            nn.ReLU(),
+            nn.Sigmoid()
         )
-        
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d): # if m is the convolutional layer, init it
+                nn.init.kaiming_normal_(m.weight, mode = 'fan_out', nonlinearity = 'relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5 * logvar)
         eps = torch.randn_like(std)
         z = mu + std * eps
         return z
-    
+
     def bottleneck(self, h):
         mu, logvar = self.fc1(h), self.fc2(h)
         z = self.reparameterize(mu, logvar)
         return z, mu, logvar
-        
+
     def representation(self, x):
         return self.bottleneck(self.encoder(x))[0]
 
@@ -125,7 +137,7 @@ class VAE(nn.Module):
         return self.decoder(z), mu, logvar
 
 def show_reconstructed(recon_x, x, cur_epoch):
-    recon_x = recon_x / 2 + 0.6
+    recon_x = recon_x / 2 + 0.5
     x = x / 2 + 0.5
 
     npimg_recon_x = recon_x.cpu().detach().numpy()
@@ -134,71 +146,60 @@ def show_reconstructed(recon_x, x, cur_epoch):
     plt.imshow(np.transpose(npimg_recon_x, (1, 2, 0)))
     plt.title('Reconstructed')
     #plt.show()
-    plt.savefig(str(N_LEARN_RATE) + '_' + str(N_BATCH_SIZE) + '_' + str(cur_epoch) + '_' + 'recon_x' + '.png', dpi = 400)
+    plt.savefig(str(N_LEARN_RATE) + '_' + str(N_BATCH_SIZE) + '_' + str(cur_epoch) + '_' + 'recon_x' + '.png', dpi = 300)
 
     plt.imshow(np.transpose(npimg_x, (1, 2, 0)))
     plt.title('Ground Truth')
     #plt.show()
-    plt.savefig(str(N_LEARN_RATE) + '_' + str(N_BATCH_SIZE) + '_' + str(cur_epoch) + '_' + 'x' + '.png', dpi = 400)
+    plt.savefig(str(N_LEARN_RATE) + '_' + str(N_BATCH_SIZE) + '_' + str(cur_epoch) + '_' + 'x' + '.png', dpi = 300)
 
 def show_generated(x, cur_epoch):
-    x = x / 2 + 0.6
+    x = x / 2 + 0.5
 
     npimg_x = x.cpu().detach().numpy()
 
     plt.imshow(np.transpose(npimg_x, (1, 2, 0)))
     plt.title('Generated')
     #plt.show()
-    plt.savefig(str(N_LEARN_RATE) + '_' + str(N_BATCH_SIZE) + '_' + str(cur_epoch) + '_' + 'gen' + '.png', dpi = 400)
+    plt.savefig(str(N_LEARN_RATE) + '_' + str(N_BATCH_SIZE) + '_' + str(cur_epoch) + '_' + 'gen' + '.png', dpi = 300)
 
 def loss_function(recon_x, x, mu, logvar):
-    # recon_x is the reconstructed tensor(or image)
-    # sum up BCE
-    # BCE = F.binary_cross_entropy(recon_x, x.view(-1, N_IMG_SIZE * N_IMG_SIZE), reduction = 'sum')
     mse_loss = nn.MSELoss(reduction = 'mean')
-    #print('recon_x shape ', recon_x.shape)
-    #print('x shape ', x.shape)
     MSE = mse_loss(recon_x, x)
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
     return MSE + KLD
 
 def train(train_loader, model, optimizer, cur_epoch, device):
-    model = model.to(device)
-    model.train()
     train_loss = 0
     inputs = None
     recon_batch = None
 
     for i, data in enumerate(train_loader, 0):
+        inputs = Variable(inputs)
         inputs, labels = data
         inputs = inputs.to(device)
 
         optimizer.zero_grad()
         recon_batch, mu, logvar = model(inputs) # return: batch reconstructed vector, mean and stdev
-        #print('input dim ', inputs.shape)
-        #print('recon_batch dim ', recon_batch.shape)
-
         loss = loss_function(recon_batch, inputs, mu, logvar) # data is the ground truth
         loss.backward()
-        optimizer.step()
         train_loss += loss.item()
+        optimizer.step()
 
     print('Epoch %5d loss: %.3f' %(cur_epoch, float(train_loss)))
-    if cur_epoch != 0 and cur_epoch % 5 == 0 and inputs is not None and recon_batch is not None:
+    if cur_epoch != 0 and cur_epoch % 2 == 0 and inputs is not None and recon_batch is not None:
         ##### RECONSTRUCTED ######
-        recon_batch = recon_batch.view(N_BATCH_SIZE, 3, N_IMG_SIZE, N_IMG_SIZE)
-        #print('input dim ', inputs)
-        #print('recon_batch dim ', recon_batch)
-        show_reconstructed(torchvision.utils.make_grid(recon_batch) \
-                , torchvision.utils.make_grid(inputs), cur_epoch)
+
+        torchvision.utils.save_image(inputs, str(N_LEARN_RATE) + '_' + str(N_BATCH_SIZE) + '_' + str(cur_epoch) + '_' + 'x' + '.png')
+        torchvision.utils.save_image(recon_batch, str(N_LEARN_RATE) + '_' + str(N_BATCH_SIZE) + '_' + str(cur_epoch) + '_' + 'recon_x' + '.png')
 
         ##### RANDOM GEN ######
         randn_noise = torch.randn(N_BATCH_SIZE, 3, N_IMG_SIZE, N_IMG_SIZE)
         generated_imgs, _, _ = model(randn_noise)
-        generated_imgs = generated_imgs.view(N_BATCH_SIZE, 3, N_IMG_SIZE, N_IMG_SIZE)
-        show_generated(torchvision.utils.make_grid(generated_imgs), cur_epoch)
+        torchvision.utils.save_image(generated_imgs, str(N_LEARN_RATE) + '_' + str(N_BATCH_SIZE) + '_' + str(cur_epoch) + '_' + 'gen' + '.png')
+        #show_generated(torchvision.utils.make_grid(generated_imgs), cur_epoch)
 
-    return float(train_loss) / float(N_BATCH_SIZE)
+    return float(train_loss)
 
 ########## MAIN #########
 if __name__ == '__main__':
@@ -230,7 +231,6 @@ if __name__ == '__main__':
         print('Train with CUDA ')
         device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         model = model.to(device)
-        model = model.cuda()
         model = torch.nn.DataParallel(model)
 
     print('Start training, N_BATCH_SIZE = %4d, N_EPOCH_LIMIT = %4d, N_LEARN_RATE %f\n' %(N_BATCH_SIZE, N_EPOCH_LIMIT, N_LEARN_RATE))
