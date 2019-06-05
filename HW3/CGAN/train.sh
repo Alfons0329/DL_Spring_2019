40c831731421c4f427a0ffc1aa827b85e4b838c4
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-06-05 14:39:09 +0800

    CGAN able to run, now tuning

--- HW3/CGAN/train.sh
+++ HW3/CGAN/train.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 set -ex
 echo $#
-if [ $# -ne 1 ];
+if [ $# -ne 3 ];
 then
-    echo "Usage: ./train.sh {date}"
+    echo "Usage: ./train.sh {date} {epochs} {batch_size}"
     exit
 fi
 
-read -p "1: Train, 2: Test " what
+read -p "1: Train + test, 2: Test only" what
 
 mkdir -p graph
 mkdir -p graph/$1
 rm -rf output/*
+
 if [ $what -eq 1 ];
 then
 	for lr in 0.0002
 	do
-		python3 cyclegan_train.py --cuda --lr $lr
+		python3 cyclegan_train.py --cuda --lr $lr --epochs $2 --batch_size $3
         mv *\_dis\.png graph/$1
         mv *\_gen\.png graph/$1
+		python3 cyclegan_test.py --cuda --lr $lr --batch_size 10 --output_imgs 10
     done
 else
 	for lr in 0.0002
 	do
-		python3 cyclegan_test.py --cuda --lr $lr
+		python3 cyclegan_test.py --cuda --lr $lr --batch_size 10 --output_imgs 10
 	done
 fi
